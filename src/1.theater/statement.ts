5e05a7236ba0bf23551da8dc7d056b0501744043
Yuta Kiyama <albero.montagna@gmail.com>
2021-07-12 20:33:38 +0900

    「変数のインライン化」

--- src/1.theater/statement.ts
+++ src/1.theater/statement.ts
@@ -32,15 +32,14 @@ function statement(invoice: Invoice, plays: { [playID: string]: Play }) {
   }).format;
 
   for (const perf of invoice.performances) {
-    const play = playFor(perf);
-    const thisAmount = amountFor(perf, play);
+    const thisAmount = amountFor(perf, playFor(perf));
 
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 喜劇のときは10人につき、さらにポイントを加算
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
     // 注文の内訳を出力
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
 
