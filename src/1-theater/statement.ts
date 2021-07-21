c15023f2ca27436eb4fe64deab8967b89d67e064
Yuta Kiyama <albero.montagna@gmail.com>
2021-07-21 13:23:22 +0900

    計算結果HTMLを作る処理を追加した

--- src/1-theater/statement.ts
+++ src/1-theater/statement.ts
@@ -21,6 +21,24 @@ function renderPlainText(data: StatementData) {
   return result;
 }
 
+function htmlStatement(invoice: InvoiceRecord, plays: PlaysRecord) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data: StatementData) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
+  for (const perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += '</table>\n';
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
 function usd(aNumber: number) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -30,3 +48,4 @@ function usd(aNumber: number) {
 }
 
 console.log(statement(invoice, plays));
+console.log(htmlStatement(invoice, plays));
